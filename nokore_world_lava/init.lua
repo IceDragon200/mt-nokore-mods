087b878b51325f7959aa23d570e5de2023da2e82
Corey Powell <mistdragon100@gmail.com>
2020-07-02 00:11:20 -0500

    nokore_world_lava: Stub

---
+++ nokore_world_lava/init.lua
@@ -0,0 +1,6 @@
+--
+--
+--
+local mod = nokore.new_module("nokore_world_lava", "0.1.0")
+
+dofile(mod.modpath .. "/nodes.lua")
---
+++ nokore_world_lava/nodes.lua
@@ -0,0 +1,1 @@
+dofile(nokore_world_lava.modpath .. "/nodes/lava.lua")
