4486cac057065ac04fab7ea1105bc32100eff416
Corey Powell <mistdragon100@gmail.com>
2020-07-02 00:11:43 -0500

    nokore_world_clay: TODOs

--- nokore_world_clay/init.lua
+++ nokore_world_clay/init.lua
@@ -1,4 +1,4 @@
-local mod = nokore.new_module("nokore_world_clay")
+local mod = nokore.new_module("nokore_world_clay", "0.1.0")
 
 dofile(mod.modpath .. "/nodes/clay.lua")
 dofile(mod.modpath .. "/nodes/baked_clay.lua")
