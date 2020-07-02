a64711214640b8978f94010f25c98e9a719850b9
Corey Powell <mistdragon100@gmail.com>
2020-07-02 00:12:44 -0500

    nokore_mapgen: A base module for clearing mapgen related stuff
    All mapgen mods should require this before attempting to register

---
+++ nokore_mapgen/init.lua
@@ -0,0 +1,10 @@
+--
+-- NoKore - Mapgen
+--
+-- This module handles clearing the core mapgen before allowing the other mapgen mods from updating it
+local mod = nokore.new_module("nokore_mapgen", "0.1.0")
+
+-- Clear existing registration
+minetest.clear_registered_biomes()
+minetest.clear_registered_ores()
+minetest.clear_registered_decorations()
