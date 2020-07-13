229b00d227ee1ad4576be42480ef774d2e1af0e8
Corey Powell <mistdragon100@gmail.com>
2020-07-13 13:52:29 -0500

    nokore_world_vine: Added vine block

--- nokore_world_vine/init.lua
+++ nokore_world_vine/init.lua
@@ -2,3 +2,5 @@
 -- NoKore - World - Vine
 --
 local mod = nokore.new_module("nokore_world_vine", "0.1.0")
+
+dofile(mod.modpath .. "/nodes/vine.lua")
---
+++ nokore_world_vine/nodes/vine.lua
@@ -0,0 +1,32 @@
+local mod = nokore_world_vine
+
+mod:register_node("vine", {
+  description = mod.S("Vine"),
+
+  groups = {
+    oddly_breakable_by_hand = 3,
+    vine = 1,
+    snappy = 3,
+    flammable = 2,
+  },
+
+  drawtype = "signlike",
+  tiles = {
+    "world_vine.png",
+  },
+  inventory_image = "world_vine.png",
+  wield_image = "world_vine.png",
+
+  paramtype = "light",
+  paramtype2 = "wallmounted",
+
+  sunlight_propagates = true,
+  walkable = false,
+  climbable = true,
+  is_ground_content = false,
+  selection_box = {
+    type = "wallmounted",
+  },
+
+  sounds = nokore.node_sounds:build("leaves"),
+})
