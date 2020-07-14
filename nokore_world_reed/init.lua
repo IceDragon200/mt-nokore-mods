d7a021c6720b40034f5125dae52eca4cd54e8c43
Corey Powell <mistdragon100@gmail.com>
2020-07-13 21:40:23 -0500

    nokore_world_reeds: Reed node

--- nokore_world_reed/init.lua
+++ nokore_world_reed/init.lua
@@ -2,3 +2,5 @@
 -- NoKore - World - Reed
 --
 local mod = nokore.new_module("nokore_world_reed", "0.1.0")
+
+dofile(mod.modpath .. "/nodes/reed.lua")
---
+++ nokore_world_reed/nodes/reed.lua
@@ -0,0 +1,30 @@
+local mod = nokore_world_reed
+
+mod:register_node("reed", {
+  description = mod.S("Papyrus"),
+
+  groups = {
+    snappy = 3,
+    flammable = 2,
+  },
+
+  drawtype = "plantlike",
+  tiles = {"default_papyrus.png"},
+  inventory_image = "default_papyrus.png",
+  wield_image = "default_papyrus.png",
+
+  selection_box = {
+    type = "fixed",
+    fixed = {-6 / 16, -0.5, -6 / 16, 6 / 16, 0.5, 6 / 16},
+  },
+
+  paramtype = "light",
+  sunlight_propagates = true,
+  walkable = false,
+  sounds = default.node_sound_leaves_defaults(),
+
+  after_dig_node = function (pos, node, metadata, digger)
+    --default.dig_up(pos, node, digger)
+    -- TODO:
+  end,
+})
