d5a38192a4048bf2aa87e831e00a2bb8675184d7
Corey Powell <mistdragon100@gmail.com>
2020-07-16 19:39:38 -0500

    nokore_chest: Create simple oak chest

--- nokore_chest/nodes/chests.lua
+++ nokore_chest/nodes/chests.lua
@@ -1,5 +1,30 @@
 local mod = nokore_chest
 
-mod:register_node("chest_oak", {
+function nokore_chest.chest_on_construct(pos)
+  local meta = minetest.get_meta(pos)
+  local inv = meta:get_inventory()
 
+  inv:set_size("main", 10 * 4)
+end
+
+mod:register_node("chest_oak_closed", {
+  description = mod.S("Oak Chest"),
+
+  groups = {
+    cracky = 1,
+    chest = 1,
+    wood_chest = 1,
+  },
+
+  tiles = {
+    "chest_top.png",
+    "chest_top.png",
+    "chest_side.png",
+    "chest_side.png",
+    "chest_front.png",
+    "chest_inside.png",
+  },
+  drawtype = "node",
+
+  on_construct = nokore_chest.chest_on_construct,
 })
