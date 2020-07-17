78a7b20b96ee845286d2df35ba4f24597581a2e9
Corey Powell <mistdragon100@gmail.com>
2020-07-16 20:54:52 -0500

    nokore_mapgen_lava: Fixed double registration issue with mapgen lava

--- nokore_mapgen_lava/init.lua
+++ nokore_mapgen_lava/init.lua
@@ -1,8 +1,8 @@
 --
--- NoKore - Mapgen Snow
+-- NoKore - Mapgen Lava
 --
--- Adds the snow nodes as mapgen aliases
-local mod = nokore.new_module("nokore_mapgen_snow", "0.1.0")
+-- Adds the lava nodes as mapgen aliases
+local mod = nokore.new_module("nokore_mapgen_lava", "0.1.0")
 
 -- Mapgen V6
 minetest.register_alias("mapgen_lava_source", "nokore_world_lava:lava_source")
