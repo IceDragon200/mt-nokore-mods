13027365fbda001672036a1e96a3a13bdd4e9f74
Corey Powell <mistdragon100@gmail.com>
2020-07-25 15:55:03 -0500

    nokore_mapgen_decor_tree_spruce: Changed tree gen a bit

--- nokore_mapgen_decor_tree_spruce/init.lua
+++ nokore_mapgen_decor_tree_spruce/init.lua
@@ -5,7 +5,49 @@ local mod = nokore.new_module("nokore_mapgen_decor_tree_spruce", "0.1.0")
 
 --dofile(mod.modpath .. "/schematics.lua")
 
-local seed = nokore_mapgen.tree_seed
+local seed = 2 -- nokore_mapgen.tree_seed
+
+minetest.register_decoration({
+  name = "nokore_mapgen_decor_tree_spruce:large_spruce_tree",
+  deco_type = "schematic",
+  place_on = {"nokore_world_snow:dirt_with_snow", "nokore_world_standard:dirt_with_coniferous_litter"},
+  sidelen = 16,
+  noise_params = {
+    offset = 0.012,
+    scale = 0.070,
+    spread = {x = 250, y = 250, z = 250},
+    seed = seed,
+    octaves = 3,
+    persist = 0.66
+  },
+  biomes = {"taiga", "coniferous_forest"},
+  y_max = 31000,
+  y_min = 4,
+  schematic = mod.modpath .. "/schematics/nokore_spruce_large_tree.mts",
+  flags = "place_center_x, place_center_z",
+  rotation = "random",
+})
+
+minetest.register_decoration({
+  name = "nokore_mapgen_decor_tree_spruce:tall_spruce_tree",
+  deco_type = "schematic",
+  place_on = {"nokore_world_snow:dirt_with_snow", "nokore_world_standard:dirt_with_coniferous_litter"},
+  sidelen = 16,
+  noise_params = {
+    offset = 0.012,
+    scale = -0.070,
+    spread = {x = 250, y = 250, z = 250},
+    seed = seed,
+    octaves = 3,
+    persist = 0.66
+  },
+  biomes = {"taiga", "coniferous_forest"},
+  y_max = 31000,
+  y_min = 4,
+  schematic = mod.modpath .. "/schematics/nokore_spruce_tall_tree.mts",
+  flags = "place_center_x, place_center_z",
+  rotation = "random",
+})
 
 minetest.register_decoration({
   name = "nokore_mapgen_decor_tree_spruce:spruce_tree",
--- nokore_mapgen_decor_tree_spruce/schematics.lua
+++ nokore_mapgen_decor_tree_spruce/schematics.lua
@@ -1,114 +1,98 @@
+local b = nokore.schematic_helpers.build()
+local v = vector.new
+
 local _ = {name = "air", prob = 0}
-local Y = {name = "nokore_world_tree_spruce:spruce_log", prob = 255, force_place = true}
-local L = {name = "nokore_world_tree_spruce:spruce_leaves", prob = 255}
-local l = {name = "nokore_world_tree_spruce:spruce_leaves", prob = 233}
+local Y = b:add_palette_node({name = "nokore_world_tree_spruce:spruce_log", prob = 255, force_place = true})
+local L = b:add_palette_node({name = "nokore_world_tree_spruce:spruce_leaves", prob = 255})
+local l = b:add_palette_node({name = "nokore_world_tree_spruce:spruce_leaves", prob = 233})
 
-local schematic = nokore.schematic_helpers.from_y_slices{
-  size = { x = 3, y = 7, z = 3 },
-  data = {
-    -- base
-    _,_,_,
-    _,Y,_,
-    _,_,_,
+local function fill_plus(bld, size, y, swatch_id)
+  for z = 0,size do
+    for x = 0,size do
+      if (x + z) <= size then
+        bld:put_node(v(x,y,z), swatch_id)
+        bld:put_node(v(-x,y,z), swatch_id)
+        bld:put_node(v(x,y,-z), swatch_id)
+        bld:put_node(v(-x,y,-z), swatch_id)
+      end
+    end
+  end
+end
 
-    _,_,_,
-    _,Y,_,
-    _,_,_,
+-- Small Spruce
+b:clear_data()
+fill_plus(b, 1, 3, l)
+b:fill_range(v(-2,4,-2), v(2,4,2), L)
+fill_plus(b, 1, 5, l)
+b:fill_range(v(-1,7,-1), v(1,7,1), L)
+b:put_node(v(0,8,0), L)
+b:fill_range(v(0,0,0), v(0,7,0), Y)
 
-    _,l,_,
-    l,Y,l,
-    _,l,_,
-
-    L,L,L,
-    L,Y,L,
-    L,L,L,
-
-    L,L,L,
-    L,L,L,
-    L,L,L,
-
-    L,L,L,
-    L,L,L,
-    L,L,L,
-
-    _,l,_,
-    l,L,l,
-    _,l,_,
-  },
-  yslice_prob = {
-    {ypos = 2, prob = 127},
-  },
+local schematic = b:to_schematic()
+schematic.yslice_prob = {
+  {ypos = 2, prob = 127},
 }
 
 local result = minetest.serialize_schematic(schematic, 'mts', {})
-
-print(dump(result))
-
 minetest.safe_file_write(minetest.get_worldpath() .. "/nokore_spruce_small_tree.mts", result)
 
-local schematic = nokore.schematic_helpers.from_y_slices{
-  size = { x = 5, y = 9, z = 5 },
-  data = {
-    -- base
-    _,_,_,_,_,
-    _,_,_,_,_,
-    _,_,Y,_,_,
-    _,_,_,_,_,
-    _,_,_,_,_,
+-- Really Tall Spruce
+b:clear_data()
+fill_plus(b, 1, 7, l)
+b:fill_range(v(-3,8,-3), v(3,8,3), L)
+b:fill_range(v(-2,9,-2), v(2,9,2), L)
+b:fill_range(v(-1,10,-1), v(1,10,1), L)
+b:fill_range(v(0,0,0), v(0,9,0), Y)
 
-    _,_,_,_,_,
-    _,_,_,_,_,
-    _,_,Y,_,_,
-    _,_,_,_,_,
-    _,_,_,_,_,
-
-    _,_,_,_,_,
-    _,_,_,_,_,
-    _,_,Y,_,_,
-    _,_,_,_,_,
-    _,_,_,_,_,
-
-    _,_,_,_,_,
-    _,_,l,_,_,
-    _,l,Y,l,_,
-    _,_,l,_,_,
-    _,_,_,_,_,
-
-    _,_,_,_,_,
-    _,L,L,L,_,
-    _,L,Y,L,_,
-    _,L,L,L,_,
-    _,_,_,_,_,
-
-    l,L,L,L,l,
-    L,L,L,L,L,
-    L,L,L,L,L,
-    L,L,L,L,L,
-    l,L,L,L,l,
-
-    l,L,L,L,l,
-    L,L,L,L,L,
-    L,L,L,L,L,
-    L,L,L,L,L,
-    l,L,L,L,l,
-
-    _,_,_,_,_,
-    _,l,L,l,_,
-    _,L,L,L,_,
-    _,l,L,l,_,
-    _,_,_,_,_,
-
-    _,_,_,_,_,
-    _,_,l,_,_,
-    _,l,l,l,_,
-    _,_,l,_,_,
-    _,_,_,_,_,
-  },
-  yslice_prob = {
-    {ypos = 2, prob = 127},
-  },
+local schematic = b:to_schematic()
+schematic.yslice_prob = {
+  {ypos = 2, prob = 127},
+  {ypos = 3, prob = 97},
 }
 
 local result = minetest.serialize_schematic(schematic, 'mts', {})
+minetest.safe_file_write(minetest.get_worldpath() .. "/nokore_spruce_tall_tree.mts", result)
 
+-- Medium Spruce
+b:clear_data()
+fill_plus(b, 1, 3, l) -- optional ring
+fill_plus(b, 3, 3, L) -- major
+fill_plus(b, 2, 4, L) -- minor
+fill_plus(b, 3, 5, L) -- major
+fill_plus(b, 2, 6, L) -- minor
+fill_plus(b, 1, 7, L) -- minor
+b:put_node(v(0,8,0), L) -- top-top
+
+local schematic = b:to_schematic()
+schematic.yslice_prob = {
+  {ypos = 2, prob = 127},
+}
+
+local result = minetest.serialize_schematic(schematic, 'mts', {})
 minetest.safe_file_write(minetest.get_worldpath() .. "/nokore_spruce_tree.mts", result)
+
+
+-- Large Spruce
+b:clear_data()
+fill_plus(b, 1, 3, l) -- optional ring
+fill_plus(b, 4, 4, L) -- largest base ring
+fill_plus(b, 2, 5, L) -- minor
+fill_plus(b, 3, 6, L) -- next major ring
+fill_plus(b, 2, 7, L) -- minor
+fill_plus(b, 3, 8, L) -- next major ring
+fill_plus(b, 2, 9, L) -- major
+fill_plus(b, 1, 10, L) -- minor
+fill_plus(b, 2, 9, L) -- major
+fill_plus(b, 1, 10, L) -- minor
+-- space
+fill_plus(b, 1, 12, L) -- top
+b:put_node(v(0,13,0), L) -- the cream of the crop
+b:fill_range(v(0,0,0), v(0,12,0), Y)
+
+local schematic = b:to_schematic()
+schematic.yslice_prob = {
+  {ypos = 2, prob = 127},
+}
+
+local result = minetest.serialize_schematic(schematic, 'mts', {})
+minetest.safe_file_write(minetest.get_worldpath() .. "/nokore_spruce_large_tree.mts", result)
