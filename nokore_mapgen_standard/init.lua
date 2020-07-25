236ca1c32f22b891ba95b9614970501a673b034d
Corey Powell <mistdragon100@gmail.com>
2020-07-25 15:55:30 -0500

    nokore_mapgen_standard: Blob dirt and gravel

--- nokore_mapgen_standard/init.lua
+++ nokore_mapgen_standard/init.lua
@@ -16,3 +16,53 @@ minetest.register_alias("mapgen_desert_stone", "nokore_world_standard:desert_sto
 minetest.register_alias("mapgen_desert_sand", "nokore_world_standard:desert_sand")
 minetest.register_alias("mapgen_cobble", "nokore_world_standard:cobblestone")
 minetest.register_alias("mapgen_mossycobble", "nokore_world_standard:mossy_cobblestone")
+
+minetest.register_ore({
+  ore_type        = "blob",
+  ore             = "nokore_world_standard:dirt",
+  wherein         = {"nokore_world_standard:stone"},
+  clust_scarcity  = 16 * 16 * 16,
+  clust_size      = 5,
+  y_max           = 31000,
+  y_min           = -31,
+  noise_threshold = 0.0,
+  noise_params    = {
+    offset = 0.5,
+    scale = 0.2,
+    spread = {x = 5, y = 5, z = 5},
+    seed = 17676,
+    octaves = 1,
+    persist = 0.0
+  },
+  -- Only where nokore_world_standard:dirt is present as surface material
+  biomes = {
+    "taiga",
+    "snowy_grassland",
+    "grassland",
+    "coniferous_forest",
+    "deciduous_forest",
+    "deciduous_forest_shore",
+    "rainforest",
+    "rainforest_swamp"
+  }
+})
+
+-- Gravel
+minetest.register_ore({
+  ore_type        = "blob",
+  ore             = "nokore_world_standard:gravel",
+  wherein         = {"nokore_world_standard:stone"},
+  clust_scarcity  = 16 * 16 * 16,
+  clust_size      = 5,
+  y_max           = 31000,
+  y_min           = -31000,
+  noise_threshold = 0.0,
+  noise_params    = {
+    offset = 0.5,
+    scale = 0.2,
+    spread = {x = 5, y = 5, z = 5},
+    seed = 766,
+    octaves = 1,
+    persist = 0.0
+  },
+})
