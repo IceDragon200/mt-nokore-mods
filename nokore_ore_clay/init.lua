aa9a90541d29de80a4546ddb332ba2ef68686a37
Corey Powell <mistdragon100@gmail.com>
2020-07-25 23:33:18 -0500

    nokore_ore_{clay,coal}: Added

---
+++ nokore_ore_clay/init.lua
@@ -0,0 +1,24 @@
+--
+-- NoKore - Ore / Clay
+--
+-- Adds clay ores to the world gen
+local mod = nokore.new_module("nokore_ore_clay", "0.1.0")
+
+minetest.register_ore({
+  ore_type        = "blob",
+  ore             = "nokore_world_clay:block",
+  wherein         = {"nokore_world_standard:sand"},
+  clust_scarcity  = 16 * 16 * 16,
+  clust_size      = 5,
+  y_max           = 0,
+  y_min           = -15,
+  noise_threshold = 0.0,
+  noise_params    = {
+    offset = 0.5,
+    scale = 0.2,
+    spread = {x = 5, y = 5, z = 5},
+    seed = -316,
+    octaves = 1,
+    persist = 0.0
+  },
+})
---
+++ nokore_ore_coal/init.lua
@@ -0,0 +1,39 @@
+--
+-- NoKore - Ore / Coal
+--
+-- Adds clay ores to the world gen
+local mod = nokore.new_module("nokore_ore_coal", "0.1.0")
+
+-- Coal
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "nokore_world_coal:stone_with_coal",
+  wherein        = "nokore_world_standard:stone",
+  clust_scarcity = 8 * 8 * 8,
+  clust_num_ores = 9,
+  clust_size     = 3,
+  y_max          = 31000,
+  y_min          = 1025,
+})
+
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "nokore_world_coal:stone_with_coal",
+  wherein        = "nokore_world_standard:stone",
+  clust_scarcity = 8 * 8 * 8,
+  clust_num_ores = 8,
+  clust_size     = 3,
+  y_max          = 64,
+  y_min          = -127,
+})
+
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "nokore_world_coal:stone_with_coal",
+  wherein        = "nokore_world_standard:stone",
+  clust_scarcity = 12 * 12 * 12,
+  clust_num_ores = 30,
+  clust_size     = 5,
+  y_max          = -128,
+  y_min          = -31000,
+})
