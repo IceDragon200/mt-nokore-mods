a2a3e3f81eb19b4e9accb052412d0a82cb073c2c
Corey Powell <mistdragon100@gmail.com>
2020-09-28 22:55:04 -0500

    nokore_backpacks: Stub

---
+++ nokore_backpacks/init.lua
@@ -0,0 +1,11 @@
+--
+-- NoKore - Backpacks
+--
+-- Backpacks are simple portable storage blocks,
+-- they can also be equipped to expand a player's inventory.
+--
+local mod = foundation.new_module("nokore_backpacks", "0.1.0")
+
+dofile(mod.modpath .. "/api.lua")
+
+dofile(mod.modpath .. "/nodes/backpack.lua")
