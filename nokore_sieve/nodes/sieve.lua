e0f60594d5444d5c5cabf07e9cda923098ddec04
Corey Powell <mistdragon100@gmail.com>
2020-09-29 22:34:30 -0500

    nokore_sieve: Added sieve textures and all variants

--- nokore_sieve/nodes/sieve.lua
+++ nokore_sieve/nodes/sieve.lua
@@ -1,6 +1,49 @@
 local mod = nokore_sieve
 
-mod:register_node("oak_sieve", {
-  description = mod.S("Oak Sieve"),
+local wood = {
+  acacia = "Acacia",
+  big_oak = "Big Oak",
+  birch = "Birch",
+  fir = "Fir",
+  jungle = "Jungle",
+  oak = "Oak",
+  sakura = "Sakura",
+  spruce = "Spruce",
+  willow = "Willow",
+}
 
-})
+for basename,name in pairs(wood) do
+  mod:register_node(basename .. "_sieve", {
+    description = mod.S(name .. " Sieve"),
+
+    drawtype = "nodebox",
+    node_box = {
+      type = "fixed",
+      fixed = {
+        -- corners
+        {-1/2, 2/16, -1/2, 1/2, 1/2, -7/16}, -- horz top
+        {-1/2, 2/16,  7/16, 1/2, 1/2,  1/2}, -- horz bottom
+        {-1/2, 2/16, -1/2, -7/16, 1/2, 1/2}, -- vert left
+        {7/16, 2/16, -1/2, 1/2, 1/2, 1/2}, -- vert right
+
+        {-7/16, 6/32, -7/16, 7/16, 7/32, 7/16}, -- mesh
+        -- legs
+        {-7/16, -1/2, -7/16, -6/16, 1/2, -6/16},
+        {6/16, -1/2, -6/16, 7/16, 1/2, -7/16},
+        {-7/16, -1/2, 6/16, -6/16, 1/2, 7/16},
+        {6/16, -1/2, 6/16, 7/16, 1/2, 7/16},
+      }
+    },
+    use_texture_alpha = true,
+    tiles = {
+      "nokore_sieve_wood_" .. basename .. "_top.png",
+      "nokore_sieve_wood_" .. basename .. "_top.png",
+      "nokore_sieve_wood_" .. basename .. "_side.png",
+      "nokore_sieve_wood_" .. basename .. "_side.png",
+      "nokore_sieve_wood_" .. basename .. "_side.png",
+      "nokore_sieve_wood_" .. basename .. "_side.png",
+    },
+
+    paramtype = "light",
+  })
+end
