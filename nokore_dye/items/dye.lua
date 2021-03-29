d8380b9cd2f2383631459c69d92875f07465a989
Corey Powell <mistdragon100@gmail.com>
2021-03-29 01:09:06 -0500

    nokore_dye: Basename for dyes

--- nokore_dye/items/dye.lua
+++ nokore_dye/items/dye.lua
@@ -1,7 +1,10 @@
 local mod = nokore_dye
 for _, color in ipairs(nokore_dye.colors) do
   mod:register_craftitem("dye_" .. color.name, {
-    description = color.description .. " Dye",
+    basename = "nokore_dye:dye",
+    base_description = mod.S("Dye"),
+
+    description = mod.S(color.description .. " Dye"),
 
     groups = {
       dye = 1,
