105de0058ed8d3059799ec4e2f4faa30b1c092e0
Corey Powell <mistdragon100@gmail.com>
2021-10-27 12:46:25 -0500

    nokore_player_service: Added callbacks to player service

--- nokore_player_service/init.lua
+++ nokore_player_service/init.lua
@@ -9,8 +9,10 @@ local mod = foundation.new_module("nokore_player_service", "1.0.0")
 
 mod:require("player_service.lua")
 
+nokore.PlayerService = mod.PlayerService
 nokore.player_service = mod.PlayerService:new()
 
 minetest.register_on_joinplayer(nokore.player_service:method("on_player_join"))
 minetest.register_on_leaveplayer(nokore.player_service:method("on_player_leave"))
 minetest.register_globalstep(nokore.player_service:method("update"))
+minetest.register_on_shutdown(nokore.player_service:method("terminate"))
--- nokore_player_service/player_service.lua
+++ nokore_player_service/player_service.lua
@@ -1,19 +1,97 @@
+-- @namespace nokore
+
+-- @class PlayerService
 local PlayerService = foundation.com.Class:extends("nokore_player_service.PlayerService")
 local ic = PlayerService.instance_class
 
+-- @spec #initialize(): void
 function ic:initialize()
   self.m_uptime = 0
   self.m_players = {}
+  self.m_player_assigns = {}
+
+  self.m_on_player_join_cbs = {}
+  self.m_on_player_leave_cbs = {}
+
+  self.m_update_cbs = {}
 end
 
+-- @spec #terminate(): void
+function ic:terminate()
+end
+
+-- @spec #trigger_on_player_join(Player, last_login: Any): void
+function ic:trigger_on_player_join(player, last_login)
+  for _key, callback in pairs(self.m_on_player_join_cbs) do
+    callback(player, last_login)
+  end
+end
+
+-- @spec #trigger_on_player_leave(Player, timed_out: Boolean): void
+function ic:trigger_on_player_leave(player, timed_out)
+  for _key, callback in pairs(self.m_on_player_leave_cbs) do
+    callback(player, timed_out)
+  end
+end
+
+-- Register a callback to the player service for a newly joined player
+--
+-- @spec #register_on_player_join(String, Function/2): self
+function ic:register_on_player_join(name, callback)
+  self.m_on_player_join_cbs[name] = callback
+
+  return self
+end
+
+-- Register a callback t the player service for a player that has left
+--
+-- @spec #register_on_player_leave(String, Function/2): self
+function ic:register_on_player_leave(name, callback)
+  self.m_on_player_leave_cbs[name] = callback
+
+  return self
+end
+
+-- Register a update callback, this function should take the player, delta_time and assigns
+--
+-- @spec register_update(String, Function/3): self
+function ic:register_update(name, callback)
+  self.m_update_cbs[name] = callback
+
+  return self
+end
+
+-- Callback used for minetest.on_joinplayer/2
+--
+-- @spec #on_player_join(Player, last_login: Any): self
 function ic:on_player_join(player, last_login)
-  self.m_players[player:get_player_name()] = player
+  local name = player:get_player_name()
+
+  self.m_players[name] = player
+  self.m_player_assigns[name] = {}
+
+  self:trigger_on_player_join(player, last_login)
+
+  return self
 end
 
+-- Callback used for minetest.on_leaveplayer/2
+--
+-- @spec #on_player_leave(Player, timed_out: Boolean): self
 function ic:on_player_leave(player, timed_out)
-  self.m_players[player:get_player_name()] = nil
+  local name = player:get_player_name()
+
+  self.m_players[name] = nil
+  self.m_player_assigns[name] = nil
+
+  self:trigger_on_player_leave(player, timed_out)
+
+  return self
 end
 
+-- Ticking update
+--
+-- @spec #update(Float): self
 function ic:update(dt)
   self.m_uptime = self.m_uptime + dt
 
@@ -23,9 +101,19 @@ function ic:update(dt)
     self.m_players[player:get_player_name()] = player
   end
 
+  for _name, callback in pairs(self.m_update_cbs) do
+    for player_name, player in pairs(self.m_players) do
+      callback(player, dt, self.m_player_assigns[player_name])
+    end
+  end
+
   return self
 end
 
+-- Retrieve a player by their name.
+-- Returns the player, or nil if they are not in the service
+--
+-- @spec #get_player_by_name(String): Player | nil
 function ic:get_player_by_name(player_name)
   return self.m_players[player_name]
 end
