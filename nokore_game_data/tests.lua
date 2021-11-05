edb442dd5b09b9a8127e4d84754d57b544a13a5c
Corey Powell <mistdragon100@gmail.com>
2021-11-04 20:47:14 -0500

    nokore_game_data: Test marshal load and dump

--- nokore_game_data/tests.lua
+++ nokore_game_data/tests.lua
@@ -73,6 +73,67 @@ if ascii_file_pack and ascii_file_unpack then
       t3:assert_deep_eq(kv:get("table_map"), { a = 1, b = 2, c = 3})
     end)
   end)
+
+  case:describe("marshall_load|marshall_dump", function (t2)
+    t2:test("can handle serializing an empty key value store", function (t3)
+      local kv = KVStore:new()
+
+      local stream = StringBuffer:new('', 'w')
+
+      t3:refute(kv:get("key"))
+
+      kv:marshall_dump(stream)
+
+      stream:close()
+      stream:open('r')
+
+      -- create a new key value store
+      local kv = KVStore:new()
+
+      kv:marshall_load(stream)
+
+      t3:refute(kv:get("key"))
+    end)
+
+    t2:test("can handle serializing a key-value store with various values", function (t3)
+      local kv = KVStore:new()
+
+      local stream = StringBuffer:new('', 'w')
+
+      kv:put("boolean_true", true)
+      kv:put("boolean_false", false)
+
+      kv:put("n-1", -1)
+      kv:put("n1", 1)
+      kv:put("n256", 256)
+
+      kv:put("string", "Hello, World")
+
+      kv:put("empty_table", {})
+      kv:put("table_array", {1, 2, 3})
+      kv:put("table_map", { a = 1, b = 2, c = 3 })
+
+      kv:marshall_dump(stream)
+
+      stream:close()
+      stream:open('r')
+
+      -- create a new key value store
+      local kv = KVStore:new()
+
+      kv:marshall_load(stream)
+
+      t3:assert_eq(kv:get("boolean_true"), true)
+      t3:assert_eq(kv:get("boolean_false"), false)
+      t3:assert_eq(kv:get("n-1"), -1)
+      t3:assert_eq(kv:get("n1"), 1)
+      t3:assert_eq(kv:get("n256"), 256)
+      t3:assert_eq(kv:get("string"), "Hello, World")
+      t3:assert_deep_eq(kv:get("empty_table"), {})
+      t3:assert_deep_eq(kv:get("table_array"), {1, 2, 3})
+      t3:assert_deep_eq(kv:get("table_map"), { a = 1, b = 2, c = 3})
+    end)
+  end)
 end
 
 case:execute()
