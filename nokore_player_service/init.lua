ed263eb1e25e60ab4f6076425d2cb61556e9bf71
Corey Powell <mistdragon100@gmail.com>
2021-12-12 12:25:08 -0500

    nokore_player_service: Initialize nokore namespace if not already initialized

--- nokore_player_service/init.lua
+++ nokore_player_service/init.lua
@@ -9,7 +9,12 @@ local mod = foundation.new_module("nokore_player_service", "1.0.0")
 
 mod:require("player_service.lua")
 
+-- @namespace nokore
+nokore = rawget(_G, "nokore") or {}
+
+-- @alias PlayerService = nokore_player_service.PlayerService
 nokore.PlayerService = mod.PlayerService
+-- @const player_service: PlayerService
 nokore.player_service = mod.PlayerService:new()
 
 minetest.register_on_joinplayer(nokore.player_service:method("on_player_join"))
