2d43c8c0827e424aecfbce48dff010eb4888fbf9
Corey Powell <mistdragon100@gmail.com>
2022-08-05 23:37:42 -0500

    nokore_block_data: Utilize RingBuffer to reduce update workload

--- nokore_block_data/init.lua
+++ nokore_block_data/init.lua
@@ -11,5 +11,9 @@ mod:require("service.lua")
 nokore = rawget(_G, "nokore") or {}
 nokore.block_data_service = nokore.BlockDataService:new()
 
-nokore_proxy.register_globalstep("nokore_block_data.update/1", nokore.block_data_service:method("update"))
+minetest.register_on_leaveplayer(nokore.block_data_service:method("on_player_leave"))
+nokore_proxy.register_globalstep(
+  "nokore_block_data.update/1",
+  nokore.block_data_service:method("update")
+)
 minetest.register_on_shutdown(nokore.block_data_service:method("terminate"))
--- nokore_block_data/service.lua
+++ nokore_block_data/service.lua
@@ -1,3 +1,6 @@
+--
+local RingBuffer = assert(foundation.com.RingBuffer)
+
 local BlockDataService = foundation.com.Class:extends("nokore_block_data.BlockDataService")
 local ic = BlockDataService.instance_class
 
@@ -5,14 +8,19 @@ local KVStore = nokore.KVStore
 
 local Trace = foundation.com.Trace
 
-local hash_node_position = minetest.hash_node_position
+local hash_node_position = assert(minetest.hash_node_position)
+local get_connected_players = assert(minetest.get_connected_players)
 
 function ic:initialize()
   self.monotonic_time = 0
+  self.elapsed_since_last_update = 0
   self.expires_duration = 120 -- every 2 minutes
   self.persist_interval = 60 -- every minute
   self.blocks = {} -- block_id => BlockDetails
   self.expired_blocks = {} -- block_id => true
+  self.next_expire_block = RingBuffer:new()
+  self.next_persist_block = RingBuffer:new()
+  self.player_block_pos_cache = {}
   self.range = 2
 
   self.nokore_dir = minetest.get_worldpath() .. "/nokore"
@@ -70,75 +78,162 @@ function ic:reduce_blocks(acc, callback)
   return acc
 end
 
-function ic:update(delta, trace)
-  self.monotonic_time = self.monotonic_time + delta
+-- @spec #on_player_leave(player: PlayerRef, timed_out: Boolean): void
+function ic:on_player_leave(player, timed_out)
+  self.player_block_pos_cache[player:get_player_name()] = nil
+end
+
+-- @spec #update(dtime: Number, trace: Trace): void
+function ic:update(dtime, trace)
+  self.monotonic_time = self.monotonic_time + dtime
+  self.elapsed_since_last_update = self.elapsed_since_last_update + dtime
+
+  if self.elapsed_since_last_update < 1 then
+    return
+  end
+  self.elapsed_since_last_update = 0
 
   local expires_at = self.monotonic_time + self.expires_duration
 
   local pos, block_id
   local block_pos = { x = 0, y = 0, z = 0 }
   local nx, ny, nz
-  local players = minetest.get_connected_players()
-  for _, player in pairs(players) do
-    pos = player:get_pos()
+  local players = get_connected_players()
+  local player_name
+  local refresh_blocks
+  local cached
+  local item
+  local block
 
-    -- neutral x, y, z
-    nx = math.floor(pos.x / 16)
-    ny = math.floor(pos.y / 16)
-    nz = math.floor(pos.z / 16)
+  if next(players) then
+    for _, player in pairs(players) do
+      player_name = player:get_player_name()
+      pos = player:get_pos()
 
-    for y = -self.range,self.range do
-      for z = -self.range,self.range do
-        for x = -self.range,self.range do
-          block_pos.x = nx + x
-          block_pos.y = ny + y
-          block_pos.z = nz + z
-          self:upsert_or_refresh_block(block_pos)
+      -- neutral x, y, z
+      nx = math.floor(pos.x / 16)
+      ny = math.floor(pos.y / 16)
+      nz = math.floor(pos.z / 16)
+
+      cached = self.player_block_pos_cache[player_name]
+      if cached then
+        if cached.pos.x ~= nx or cached.pos.y ~= ny or cached.pos.z ~= nz then
+          refresh_blocks = true
+          cached.pos = {
+            x = nx,
+            y = ny,
+            z = nz,
+          }
+        end
+      else
+        refresh_blocks = true
+        cached = {
+          pos = {
+            x = nx,
+            y = ny,
+            z = nz,
+          }
+        }
+
+        self.player_block_pos_cache[player_name] = cached
+      end
+
+      if not cached.expires_at or cached.expires_at < self.monotonic_time then
+        -- the cache is stale, force refresh it
+        cached.expires_at = nil
+        refresh_blocks = true
+      end
+
+      if refresh_blocks then
+        for y = -self.range,self.range do
+          for z = -self.range,self.range do
+            for x = -self.range,self.range do
+              block_pos.x = nx + x
+              block_pos.y = ny + y
+              block_pos.z = nz + z
+              block = self:upsert_or_refresh_block(block_pos)
+
+              -- try setting the caches expiration based on the block's expiration
+              -- this will force the player caches to refresh the blocks after some time
+              if cached.expires_at then
+                if block.expires_at < cached.expires_at then
+                  cached.expires_at = block.expires_at
+                end
+              else
+                cached.expires_at = block.expires_at
+              end
+            end
+          end
         end
       end
     end
   end
 
-  for block_id, block in pairs(self.blocks) do
-    if block.next_persist_at < self.monotonic_time then
-      self:persist_block(block)
-    end
+  while not self.next_expire_block:is_empty() do
+    item = self.next_expire_block:peek()
+    if item.expires_at < self.monotonic_time then
+      self.next_expire_block:pop()
+      block = self.blocks[item.id]
 
-    if block.expires_at < self.monotonic_time then
-      self.expired_blocks[block_id] = true
+      if block then
+        if block.expires_at < self.monotonic_time then
+          self.expired_blocks[item.id] = true
+        else
+          self.next_expire_block:push({
+            id = item.id,
+            expires_at = block.expires_at,
+          })
+        end
+      end
+    else
+      break
     end
   end
 
-  local expired = false
-  local block
-  local span
-  for block_id,_ in pairs(self.expired_blocks) do
-    expired = true
-    block = self.blocks[block_id]
-    self.blocks[block_id] = nil
-    if trace then
-      span = trace:span_start("expired_block:" .. block_id)
-    end
-    self:persist_block(block, span)
-    if span then
-      span:span_end()
+  while not self.next_persist_block:is_empty() do
+    item = self.next_persist_block:peek()
+    if item.next_persist_at < self.monotonic_time then
+      self.next_persist_block:pop()
+      block = self.blocks[item.id]
+      if block then
+        if block.next_persist_at < self.monotonic_time then
+          self:persist_block(block)
+        end
+      end
+    else
+      break
     end
   end
 
-  if expired then
-    self.expired_blocks = {}
+  if next(self.expired_blocks) then
+    local expired = false
+    local block
+    local span
+    for block_id,_ in pairs(self.expired_blocks) do
+      expired = true
+      block = self.blocks[block_id]
+      self.blocks[block_id] = nil
+      if trace then
+        span = trace:span_start("expired_block:" .. block_id)
+      end
+      self:persist_block(block, span)
+      if span then
+        span:span_end()
+      end
+    end
+
+    if expired then
+      self.expired_blocks = {}
+    end
   end
 end
 
 function ic:upsert_or_refresh_block(block_pos)
   local id = hash_node_position(block_pos)
 
-  if self.blocks[id] then
-    local block = self.blocks[id]
-    block.expires_at = self.monotonic_time + self.expires_duration
-    return block
-  else
-    minetest.log("info", "initializing block data block_id=" .. id)
+  local block = self.blocks[id]
+  if not block then
+    -- minetest.log("debug", "initializing block data block_id=" .. id)
     local basename = "("..block_pos.x..","..block_pos.y..","..block_pos.z..")"
 
     local kv = KVStore:new()
@@ -153,7 +248,7 @@ function ic:upsert_or_refresh_block(block_pos)
       kv:apack_load_file(filename)
     end
 
-    local block = {
+    block = {
       id = id,
       pos = vector.new(block_pos), -- copy the position
       basename = basename,
@@ -165,23 +260,39 @@ function ic:upsert_or_refresh_block(block_pos)
     }
 
     self.blocks[id] = block
-    return block
+    self.next_persist_block:push({
+      id = id,
+      next_persist_at = block.next_persist_at,
+    })
   end
+
+  block.expires_at = self.monotonic_time + self.expires_duration
+  self.next_expire_block:push({
+    id = id,
+    expires_at = block.expires_at,
+  })
+
+  return block
 end
 
 function ic:persist_block(block, trace)
-  if block.kv.dirty then
-    block.kv.dirty = false
+  local kv = block.kv
+  if kv.dirty then
+    kv.dirty = false
     if self.persistance_type == 'MRSH' then
-      block.kv:marshall_dump_file(block.filename, trace)
+      kv:marshall_dump_file(block.filename, trace)
     elseif self.persistance_type == 'ASCI' then
-      block.kv:apack_dump_file(block.filename, trace)
+      kv:apack_dump_file(block.filename, trace)
     elseif self.persistance_type == 'NONE' then
       -- can't persist
     end
   end
   block.persisted_at = self.monotonic_time
   block.next_persist_at = block.persisted_at + self.persist_interval
+  self.next_persist_block:push({
+    id = block.id,
+    next_persist_at = block.next_persist_at,
+  })
 end
 
 nokore.BlockDataService = BlockDataService
