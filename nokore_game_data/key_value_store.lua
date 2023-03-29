d3d08d7d0e1e8eb84890f0ff41178a92859d36d2
Corey Powell <mistdragon100@gmail.com>
2023-03-28 21:08:58 -0500

    nokore_game_data: KeyValueStore additional functions

--- nokore_game_data/key_value_store.lua
+++ nokore_game_data/key_value_store.lua
@@ -3,10 +3,14 @@
 --
 -- Simple class for defining a key-value store
 -- @namespace nokore
+local table_keys = assert(foundation.com.table_keys)
+local table_values = assert(foundation.com.table_values)
 
 -- @class KVStore
 local KVStore = foundation.com.Class:extends("nokore.KVStore")
 local ic = KVStore.instance_class
+
+-- @type Key: String
 
 -- @type Value: Integer | String | Table | Boolean
 
@@ -39,15 +43,86 @@ function ic:get(key, default)
   return value
 end
 
+-- @spec #get_lazy(key: String, Function/0): Value
+function ic:get_lazy(key, callback)
+  local value = self.data[key]
+
+  if value == nil then
+    return callback()
+  end
+
+  return value
+end
+
+-- @spec #keys(): Key[]
+function ic:keys()
+  return table_keys(self.data)
+end
+
+-- @spec #values(): Value[]
+function ic:values()
+  return table_values(self.data)
+end
+
 -- @spec #has_key(key: String): Boolean
 function ic:has_key(key)
   return self.data[key] ~= nil
 end
 
--- @spec #put(String, Any): self
+-- @spec #put(String, Value): self
 function ic:put(key, value)
   self.data[key] = value
   self.dirty = true
+  return self
+end
+
+-- @spec #put_new(String, Value): self
+function ic:put_new(key, value)
+  if self.data[key] == nil then
+    return self:put(key, value)
+  end
+  return self
+end
+
+-- @spec #put_new_lazy(String, Function/0): self
+function ic:put_new_lazy(key, callback)
+  if self.data[key] == nil then
+    return self:put(key, callback())
+  end
+  return self
+end
+
+-- Update an key-value pair regardless of if it exists.
+--
+-- Usage:
+--
+--     upsert_lazy("my_key", function (old_value)
+--       ... do something with old_value
+--       return new_value
+--     end)
+--
+-- @spec #upsert_lazy(String, Function/1): self
+function ic:upsert_lazy(key, callback)
+  self.data[key] = callback(self.data[key])
+  self.dirty = true
+  return self
+end
+
+-- Update an existing key-value pair.
+--
+-- Usage:
+--
+--     update_lazy("my_key", function (old_value)
+--       ... do something with old_value
+--       return new_value
+--     end)
+--
+-- @spec #update_lazy(String, Function/1): self
+function ic:update_lazy(key, callback)
+  if self.data[key] ~= nil then
+    self.data[key] = callback(self.data[key])
+    self.dirty = true
+  end
   return self
 end
 
