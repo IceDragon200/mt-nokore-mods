29161460ea33b719a38a4944d7ae4bf6a86011c5
Corey Powell <mistdragon100@gmail.com>
2025-09-17 22:04:10 -0500

    nokore_common: Backfill missing functions and warn instead of crashing

--- nokore_common/init.lua
+++ nokore_common/init.lua
@@ -9,10 +9,24 @@ nokore = rawget(_G, "nokore") or {}
 
 if nokore.make_tool_cap_times and nokore.make_tool_capability and nokore.dig_class then
   -- nothing to do here
+  core.log("info", "nokore utility functions are present, skipping patches")
   return
 end
 
-if not nokore.make_tool_cap_times and not nokore.make_tool_capability and not nokore.dig_class then
+if not nokore.make_tool_cap_times then
+  --- @spec make_tool_cap_times(tool_class: String, material_class: String): { [Integer] = Number }
+  function nokore.make_tool_cap_times(tool_class, material_class)
+    error("TODO")
+  end
+end
+
+if not nokore.make_tool_capability then
+  function nokore.make_tool_capability(tool_class, material_class)
+    error("TODO")
+  end
+end
+
+if not nokore.dig_class then
   --
   -- Nokore's default tool caps are an approximation of minetest_game's default against HSW's
   -- materials.
@@ -36,15 +50,6 @@ if not nokore.make_tool_cap_times and not nokore.make_tool_capability and not no
     nano_element = 1,
   }
 
-  --- @spec make_tool_cap_times(tool_class: String, material_class: String): { [Integer] = Number }
-  function nokore.make_tool_cap_times(tool_class, material_class)
-    error("TODO")
-  end
-
-  function nokore.make_tool_capability(tool_class, material_class)
-    error("TODO")
-  end
-
   --- @spec dig_class(material_class: String): Integer
   function nokore.dig_class(material_class)
     local value = DIG_CLASS[material_class]
@@ -53,25 +58,6 @@ if not nokore.make_tool_cap_times and not nokore.make_tool_capability and not no
     end
     error("dig_class not found got=" .. material_class)
   end
-else
-  error([[
-    incomplete make_tool_* and dig_class implementations.
-
-    make_tool_cap_times, make_tool_capability, and dig_class must all be defined:
-
-        function nokore.make_tool_cap_times(tool_class, material_class)
-          return tool_cap_times
-        end
-
-        function nokore.make_tool_capability(tool_class, material_class)
-          return tool_capability
-        end
-
-        function nokore.dig_class(material_class)
-          return level
-        end
-
-    Either define all the functions, or none at all
-  ]])
 end
 
+core.log("warning", "nokore's backfilling has been completed for some core functions, they may not be what you want however.")
